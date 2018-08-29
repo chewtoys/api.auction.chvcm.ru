75eb73781441daa3d55e35c1b19ebe595328db33
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-29 06:43:21 +0000

    Deploy AlendoStudio/api.auction.chvcm.ru to github.com/AlendoStudio/api.auction.chvcm.ru.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-08-28T09:38:14.644Z",
+    "time": "2018-08-29T06:43:11.972Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
